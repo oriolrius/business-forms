7a60cc697fb85c77358b7f2ad94471bc71aeef31
Oriol Rius <oriol@joor.net>
2025-07-01 13:46:22 +0200

    refactor: Remove test file for NumInputNoLimitsElement

--- src/components/FormElement/components/NumInputNoLimitsElement/test-type.ts
+++
@@ -1,6 +0,0 @@
-import { FormElementType } from '@/types';
-
-// Test that the new type is properly defined
-console.log('NUM_INPUT_NO_LIMITS type:', FormElementType.NUM_INPUT_NO_LIMITS);
-
-export {};
